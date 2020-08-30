2e2dbadbf767bbabb365bcf37fd77c6bee4cafc9
Alexander Volkmann <alexv@gmx.de>
2020-08-30 22:30:18 +0200

    read install_requires from requirements.txt

--- setup.py
+++ setup.py
@@ -10,17 +10,6 @@ setup(
     author_email='alexv@gmx.de',
     packages=find_packages(),
     setup_requires=['pytest-runner>=4.2', 'flake8'],
-    install_requires=[
-        'numpy==1.16.3',
-        'xlrd==1.1.0',
-        'pandas==0.25.1',  # 0.23.3',
-        'pystan==2.18.1.0',
-        'scipy==1.2.1',
-        'arviz==0.3.3',
-        'statsmodels==0.10.1',
-        'matplotlib==3.1.0',
-        'jupytext==1.2.4',
-        'plotly==2.0.8'
-    ],
+    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
     tests_require=['pytest>=4.0.2', 'pytest-cov>=2.6.0', 'pytest-watch>=4.2.0']
 )
