11442f5c596cf76fbdb549a5b42d9edac160e5a5
Alexander Volkmann <alexv@gmx.de>
2020-08-30 17:10:00 +0200

    generalize get_pooled_data method

--- src/lib/pool_arms.py
+++ src/lib/pool_arms.py
@@ -15,18 +15,22 @@ def get_pooled_sd(n_i: np.array, mu_i: np.array, sd_i: np.array) -> np.array:
     )
 
 
-def get_pooled_data(df, arm_size, mean, sd):
+def get_pooled_data(df, arm_size, mean, sd=None):
     df_agg = pd.DataFrame(
         df.groupby(['study_id', 'is_active']).apply(
             lambda x: pd.DataFrame(
                 [
                     np.sum(x[arm_size]),
                     get_pooled_mean(x[arm_size], x[mean]),
-                    get_pooled_sd(x[arm_size], x[mean], x[sd]),
+                    get_pooled_sd(x[arm_size], x[mean], x[sd]) if sd else 0,
                     '-'.join(np.unique(x['scale']))
                 ]
             ).T
         )
-    ).reset_index().rename(columns={0: 'N', 1: mean, 2: sd, 3: 'scale'}).astype({'N': int, mean: float, sd: float})
+    ).reset_index()
+    if sd:
+        df_agg = df_agg.rename(columns={0: 'N', 1: mean, 2: sd, 3: 'scale'}).astype({'N': int, mean: float, sd: float})
+    else:
+        df_agg = df_agg.rename(columns={0: 'N', 1: mean, 2: 'scale'}).astype({'N': int, mean: float})
     del df_agg['level_2']
     return df_agg
