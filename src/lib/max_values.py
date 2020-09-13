5da7117d9ffc6699d749098d83b5ea24280993fa
Alexander Volkmann <alexv@gmx.de>
2020-09-13 18:45:32 +0200

    fix typo

--- src/lib/max_values.py
+++ src/lib/max_values.py
@@ -5,7 +5,7 @@ MAX_VALUES = dict(
     HAMD17=52,  # question 16 max=2, https://academic.oup.com/occmed/article/65/4/340/1377801
     HAMD21=64,
     HAMD24=75,
-    HAMD29=87,  # imputed from: HAMD24 and HAMD29
+    HAMD29=87,  # imputed from: HAMD24 and HAMD31
     HAMD31=93,  # https://link.springer.com/content/pdf/10.1007/978-1-59745-387-5.pdf, p. 48 ff.
     MADRS=60,
     HAMDunspecified=None,
